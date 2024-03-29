6303e2494b2b02f08c14feb292e35120232f46cf
Fergal Mulvey <mulvey.fergal@gmail.com>
2024-03-29 00:44:45 +0000

    fix: Division questions will now divide evenly

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,8 +92,7 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     }
     else if(operator === "/"){
-        let answer = Math.floor(operand1 / operand2);
-        return [answer, "division"];
+        return [operand1 / operand2, "division"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -137,7 +136,7 @@ function displayMultiplyQuestion(operand1, operand2){
 }
 
 function displayDivisionQuestion(operand1, operand2){
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operand1').textContent = operand1*operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operator').textContent = "/";
 }
